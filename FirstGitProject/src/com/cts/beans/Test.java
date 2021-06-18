e2b068c016e76c8f0ddd86a440fb5bbd02fe9494
Dev1@gmail.com <Ajay@AJAY-DESKTOP>
2021-06-18 10:26:31 +0530

    Devloper1 has modified Test.java

--- FirstGitProject/src/com/cts/beans/Test.java
+++ FirstGitProject/src/com/cts/beans/Test.java
@@ -3,10 +3,15 @@ package com.cts.beans;
 import java.util.Date;
 
 public class Test {
-	
+
 	public Date getSystemDate() {
-		
+
 		return new Date();
+	}
+
+	public void m1() {
+		
+		System.out.println("This is m1()");
 	}
 
 }
